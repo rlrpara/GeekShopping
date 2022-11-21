fa3ffdaf48c5172e38bed66c129ad90dcd22a5d7
Rodrigo L. Ribeiro <rodrigo.ribeiro@questores.com.br>
2022-11-21 09:30:37 -0300

    ajust

--- Src/GeekShopping.ProductApi/Controller/ProductController.cs
+++ Src/GeekShopping.ProductApi/Controller/ProductController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.ProductApi.Controller;
 
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 [ApiController]
 public class ProductController : ControllerBase
 {
